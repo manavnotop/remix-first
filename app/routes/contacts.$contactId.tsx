d118f73e3fba71771f7fcd2da2b74722bd9ff745
manavnotop <manuagarwal1443@gmail.com>
2025-01-09 14:03:37 +0530

    add loader functions

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,17 +1,23 @@
 import { Form } from "@remix-run/react";
 import type { FunctionComponent } from "react";
+import { json, LoaderFunctionArgs } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 
-import type { ContactRecord } from "../data";
+import { getContact, type ContactRecord } from "../data";
+import invariant from "tiny-invariant";
+
+export const loader = async ({ params, }: LoaderFunctionArgs) => {
+    invariant(params.contactId, "Missing contactId param");
+    const contact = await getContact(params.contactId);
+    return json({ contact });
+}
 
 export default function Contact() {
-  const contact = {
-    first: "Your",
-    last: "Name",
-    avatar: "https://placecats.com/200/200",
-    twitter: "your_handle",
-    notes: "Some notes",
-    favorite: true,
-  };
+
+  const { contact } = useLoaderData<typeof loader>();
+  if(!contact){
+    throw new Response("Not found", { status: 404 });
+  }
 
   return (
     <div id="contact">
